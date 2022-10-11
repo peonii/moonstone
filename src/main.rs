46bfd06dc1698518bb88bb66084a7ae5937c905c
peony <querter.ovutem@gmail.com>
2022-10-11 19:04:17 +0200

    workflows?

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+#![warn(clippy::unwrap_used)]
+#![warn(clippy::expect_used)]
+
+use std::fs;
+
 fn main() {
     println!("Hello, world!");
+
+    let idk = fs::read("asdfasd").unwrap();
+
+    println!("{idk:?}");
 }
